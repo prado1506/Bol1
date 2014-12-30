cafb56299397f6ed7a8e92e97b43b333d71e7bad
prado1506 <prado1506@gmail.com>
2014-12-30 20:08:53 -0200

    0.4

--- AutoLevelTyler1.lua
+++ AutoLevelTyler1.lua
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
 local version = "0.4"
-=======
--- Tyler1 Auto Level Spells --
-local version = "0.2"
- 
->>>>>>> origin/master
 local abilitySequence
 local qOff, wOff, eOff, rOff = 0,0,0,0
 
 function OnTick()
-<<<<<<< HEAD
 
     if Menu.Ads.AutoLevelspells and Menu.start then
 		Carry()
-=======
-    if Menu.Ads.AutoLevelspells then
-	Carry()
->>>>>>> origin/master
         AutoLevel()
     end
 end	
@@ -27,12 +16,8 @@ end
  
  function Carry()
     local sequence = Menu.Ads.sequenceSpells
-<<<<<<< HEAD
 	
         if sequence == 1 then        abilitySequence = { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3, }
-=======
-            if sequence == 1 then        abilitySequence = { 1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3, }
->>>>>>> origin/master
 	elseif sequence == 2 then        abilitySequence = { 1, 3, 2, 1, 1, 4, 1, 3, 1, 3, 4, 3, 3, 2, 2, 4, 2, 2, }
 	elseif sequence == 3 then        abilitySequence = { 2, 1, 3, 2, 2, 4, 2, 1, 2, 1, 4, 1, 1, 3, 3, 4, 3, 3, }
 	elseif sequence == 4 then        abilitySequence = { 2, 3, 1, 2, 2, 4, 2, 3, 2, 3, 4, 3, 3, 1, 1, 4, 1, 1, }
@@ -50,14 +35,11 @@ end
 end
 
 function OnLoad()
-<<<<<<< HEAD
 
 	PrintChat(" >> Loading Tyler1 Auto Level Spells "..version..)
-=======
->>>>>>> origin/master
 	Menu = scriptConfig("AutoLevel", player.charName)
 	Menu:addSubMenu("["..myHero.charName.." - AutoLevel]", "Ads")
-	Menu.Ads:addParam("sequenceSpells", "Sequence Spells", SCRIPT_PARAM_LIST, 1, { 'RQWE', 'RQEW', 'RWQE', 'RWEQ', 'REWQ', 'REQW' })
+    Menu.Ads:addParam("sequenceSpells", "Sequence Spells", SCRIPT_PARAM_LIST, 1, { 'RQWE', 'RQEW', 'RWQE', 'RWEQ', 'REWQ', 'REQW' })
 	Menu.Ads:addParam("AutoLevelspells", "Auto-Level Spells", SCRIPT_PARAM_ONOFF, false)
 	Menu:addParam("sep", "-- Active Script --", SCRIPT_PARAM_INFO, "")
 	Menu.addParam("start", "Auto-Level Start", SCRIPT_PARAM_ONOFF, false)
