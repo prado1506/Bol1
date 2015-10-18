f4d9104c7c280d24b3ab8655327763792667bf56
Guilherme Prado <prado1506@gmail.com>
2015-10-17 23:28:02 -0300

    Update AutoLevelSkillTyler1.lua

--- AutoLevelSkillTyler1.lua
+++ AutoLevelSkillTyler1.lua
@@ -30,7 +30,7 @@ local _autoLevel = { spellsSlots = { SPELL_1, SPELL_2, SPELL_3, SPELL_4 }, level
 local __autoLevel__OnTick
 local rOFF=0
 --update func--
-local version = "2.181"
+local version = "2.20"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/prado1506/Bol1/master/AutoLevelSkillTyler1.lua".."?rand="..math.random(1,10000)
