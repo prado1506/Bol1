f5da971f768ddc838a8b55e0341d62ddf97586b6
Guilherme Prado <prado1506@gmail.com>
2017-11-13 10:16:59 -0200

    FixGA.lua 1.0
    FixGA.lua 1.0

---
+++ FixGA.lua
@@ -0,0 +1,4 @@
+_G.ValidTarget = function(object, distance, enemyTeam)
+    local enemyTeam = (enemyTeam ~= false)
+    return object ~= nil and object.valid and (object.team ~= player.team) == enemyTeam and object.visible and not object.dead and object.bTargetable and (enemyTeam == false or object.bInvulnerable ~= 1) and (distance == nil or GetDistanceSqr(object) <= distance * distance)
+end
